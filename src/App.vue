f8462692f79db01170570fdf3814de2704dac733
unknown <cuneydbolukogluu@gmail.com>
2022-03-22 13:30:03 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -164,18 +164,28 @@ main.warm {
 
 .search-box {
   width: 100%;
-  padding: 15px;
-  border-radius: 5px;
-  border: none;
-  font-size: 25px;
+  margin-bottom: 30px;
 }
-
-.search-bar {
+.search-box .search-bar {
+  display: block;
   width: 100%;
   padding: 15px;
-  border-radius: 5px;
-  border: none;
-  font-size: 25px;
+  
+  color: #313131;
+  font-size: 20px;
+  appearance: none;
+  border:none;
+  outline: none;
+  background: none;
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0px 16px 0px;
 }
 
 .location-box .location {
